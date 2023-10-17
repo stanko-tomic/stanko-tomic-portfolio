b60385d0d35a8757c95aa6307389096ba22f3fd7
staen <stankotomic7@gmail.com>
2023-10-17 16:35:06 +0200

    add meta tags

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,6 +10,17 @@ const workSans = Work_Sans({ subsets: ["latin"] });
 const Layout: React.FC<ILayout> = (props) => {
   return (
     <>
+      <Head>
+        <meta name="title" content="hey" />
+        <meta
+          name="description"
+          content="Stanko Tomić web developer portfolio website"
+        />
+        <meta name="keywords" content="web developer,web design,portfolio" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+      </Head>
       <main className={`${workSans.className}`}>
         <NavBar />
         <section>{props.children}</section>
