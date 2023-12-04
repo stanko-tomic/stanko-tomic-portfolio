f160f847285d1ed2c57754e112304c414b849302
staen <stankotomic7@gmail.com>
2023-12-04 16:31:32 +0100

    Open graph fixes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,11 +11,33 @@ const Layout: React.FC<ILayout> = (props) => {
   return (
     <>
       <Head>
-        <meta name="title" content="Stanko Tomic | Portfolio" />
+        <title>Stanko Tomic | Portfolio</title>
         <meta
           name="description"
           content="Stanko Tomić web developer portfolio website"
         />
+
+        <meta property="og:url" content="https://stanko-tomic.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Stanko Tomic | Portfolio" />
+        <meta
+          property="og:description"
+          content="Stanko Tomić web developer portfolio website"
+        />
+        <meta property="og:image" content="/opengraph.jpg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="stanko-tomic.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="https://stanko-tomic.vercel.app/"
+        />
+        <meta name="twitter:title" content="Stanko Tomic | Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Stanko Tomić web developer portfolio website"
+        />
+        <meta name="twitter:image" content="/opengraph.jpg" />
         <meta name="keywords" content="web developer,web design,portfolio" />
         <meta name="robots" content="index, follow" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
